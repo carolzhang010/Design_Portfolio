4e1ed18f3cbd05148944f92c7aa13d426eba35f9
Carol Li Zhang <clz2116@columbia.edu>
2025-09-14 15:26:34 -0400

    added webpage content

--- src/pages/Humor.js
+++ src/pages/Humor.js
@@ -10,12 +10,12 @@ import consumerreports from "../assets/CR.png";
 function DescriptionText() {
   return (
     <p className="descriptionText">
-      text text text text text text text text text text text text text text text <br />
-      text text text text text text text text text text text text text text text <br />
-      text text text text text text text text text text text text text text text <br />
-      text text text text text text text text text text text text text text text <br />
-      text text text text text text text text text text text text text text text <br />
-      text text text text text text text text text text text text text text text <br />
+      In a project focused on front-end development and interactive web design,
+      I contributed to the creations of Crackd.ai, an AI-powered joke-generation
+      platform developed by Sparkling Javascript. My work centered on building
+      and refining the product's web experience through responsive
+      React/Next.js components and a multi-page corporate website
+      based on Figma mockups.
     </p>
   );
 }
@@ -43,18 +43,15 @@ function UIDesign() {
         UI Design
       </h2>
       <p className="sectionDescription">
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description
+        For the Humor Project, I designed and developed a range
+        of responsive UI components that shaped the platform's
+        interactive experience. Using Figma and Canva, I designed
+        different, responsive layouts and explored potential
+        functionalities, focusing on how users would navigate
+        the site and interact with its "like" functionality and
+        joke-generation. These mockups served as blueprints for
+        the final build, guiding the development of interfaces that
+        felt intuitive and aligned with the project's identity.
       </p>
     </div>
   );
@@ -96,19 +93,16 @@ function CrossTeamCollaboration() {
       <h2 className="sectionHeader">
         Cross-Team Collaboration
       </h2>
-      <p className="sectionDescription">
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description
+      <p className="sectionDescription"
+      style={{ marginTop: 0, marginRight: "auto", marginBottom: 40, marginLeft: "auto", width: "100%" }}>
+        Collaboration was a key part of my work in this project.
+        I partnered closely with the backend team to ensure that
+        API routes connected seamlessly with the front-end, enabling
+        real-time joke generation and A/B testing functionality.
+        I also worked alongside another UI designer on the corporate website,
+        translating her Figma mockups into a fully responsive front-end build.
+        In some cases, we collaborated to improve usability and clarity to
+        make the final website more user-friendly.
       </p>
     </div>
   );
@@ -121,18 +115,18 @@ function DesignImplementation() {
         Design Implementation
       </h2>
       <p className="sectionDescription">
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description
+        Once UI designs were finalized, I focused on translating
+        them into a functional, polished interface. Using React,
+        Next.js, HTML, Tailwind, CSS, and JavaScript, I implemented
+        the mockups into responsive layouts that worked across different
+        devices. Reusable components were built to streamline future
+        development, and it ensured consistency in style and functionality
+        throughout the site. Accessibility and performance were also
+        priorities - page structures were optimized so that the interface
+        was both fast and user-friendly. To maintain code quality,
+        I worked within a Github-based workflow with regular commits,
+        pull requests, and integrated link checks, ensuring the codebase
+        remained clean, maintainable, and aligned with team standards.
       </p>
     </div>
   );
@@ -173,19 +167,19 @@ function FinalThoughts() {
       <h2 className="sectionHeader">
         Final Thoughts
       </h2>
-      <p className="sectionDescription">
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description description description description
-        description
+      <p className="sectionDescription"
+      style={{ marginTop: 0, marginRight: "auto", marginBottom: 40, marginLeft: "auto", width: "100%" }}>
+         Working on the Humor Project was a valuable experience that deepened
+         my understanding of front-end development and cohesive design. I
+         gained hands-on practice in maintaining the front-end UI to seamlessly
+         connect with backend APIs, ensuring that features like real-time joke
+         generation and A/B testing functioned correctly. Beyond technical skills,
+         I grew in cross-team collaboration, coordinating with backend developers and
+         designers to develop a polished product. If I were to approach the project
+         again, I would challenge myself to dive deeper into API, prompt engineering,
+         and backend work to strengthen my full-stack perspective. Overall, the
+         project reinforced how much I enjoy bridging design and development -
+         turning creative work into functional, intuitive experiences.
       </p>
     </div>
   );
